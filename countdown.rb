5e815459f1e76ba7a2f4f581b4a2043cd5a972c6
Francisco Ezeqiel Banda <febanda91@gmail.com>
2018-11-13 21:50:56 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,8 @@
-#write your code here
-
-def countdown
+def countdown(number_of_times)
+  counter = number_of_times
+  while counter > 0
+    puts "#{counter} SECONDS(S)!"
+    break if counter == 0
+    puts "HAPPY NEW YEAR!"
+  end 
 end
