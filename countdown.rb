dea7e15749a681d4553d9f85aaa6ce5301e02119
Francisco Ezeqiel Banda <febanda91@gmail.com>
2018-11-13 22:58:02 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -5,3 +5,13 @@ def countdown(number_of_times)
   end
   return "HAPPY NEW YEAR!"
 end
+
+
+def countdown_with_sleep(number)
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    number -= 1
+    sleep 1
+  end
+  return "HAPPY NEW YEAR!"
+end 
